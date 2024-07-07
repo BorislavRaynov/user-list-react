187f205f0963549081d645b0ba4dc04a3ad8e2f6
BorislavRaynov <b_raynov@abv.bg>
2024-07-07 17:12:48 +0300

    Footer component added.

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className="footer">
+                <p>This site is designed to be used for training purposes for Borisalv Raynov.</p>
+            </footer>
+        </>
+    );
+}
