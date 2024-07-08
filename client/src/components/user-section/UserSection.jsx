994e5ca3be934a6936de201859d885244688933c
BorislavRaynov <b_raynov@abv.bg>
2024-07-08 19:21:40 +0300

    Modal state added.

--- client/src/components/user-section/UserSection.jsx
+++ client/src/components/user-section/UserSection.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import Pagination from "../pagination/Pagination";
 import Search from "../search/Search";
 import UserList from "./user-list/UserList";
+import UserAdd from "./user-add/UserAdd";
 
 const baseUrl = "http://localhost:3030/jsonstore"
 
 export default function UserSection() {
     const [users, setUsers] = useState([]);
+    const [showAddUser, setShowAddUser] = useState(false);
 
     useEffect(() => {
         (async function getUsers() {
@@ -25,14 +27,52 @@ export default function UserSection() {
         })();
     }, [])
 
+    const addUserClickHandler = () => {
+        setShowAddUser(true)
+    }
+
+    const addUserCloseHandler = () => {
+        setShowAddUser(false)
+    }
+
+    const addUserSaveHandler = async (e) => {
+        e.preventDefault()
+        
+        const formData = new FormData(e.currentTarget)
+        const userData = {
+            ...Object.fromEntries(formData),
+            createAt: new Date.toISOString(),
+            updateAt: new Date.toISOString(),
+        };
+
+        const response = await fetch(`${baseUrl}/users`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        });
+
+        const createdUser = await response.json();
+        setUsers(oldUsers => [...oldUsers, createdUser]);
+        setShowAddUser(false)
+    }
+
     return (
         <section className="card users-container">
         
             <Search />
     
             <UserList users={users} />
+
+            {showAddUser && (
+                <UserAdd 
+                    onClose={addUserCloseHandler}
+                    onSave={addUserSaveHandler}
+                />
+            )}
     
-            <button className="btn-add btn">Add new user</button>
+            <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button>
             
             <Pagination />
 
